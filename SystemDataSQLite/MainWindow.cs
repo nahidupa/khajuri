cd6b98647f5dbf0be36b80feadec8c8971fdee3e
munnaonc@gmail.com <munnaonc@gmail.com@81686834-6d73-e669-406b-a718203c53d8>
2011-07-24 09:48:20 +0000

    SQL lite

---
+++ SystemDataSQLite/MainWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SystemDataSQLite
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                NorthWindSQLiteEntities context = new NorthWindSQLiteEntities();
+                //context.DeleteObject();
+
+                //using (NorthwindEntities context = new NorthwindEntities())
+                //{
+                var result = from Customer c in context.Customers
+                             select c;
+                List<Customer> customers = result.ToList();
+                //}
+
+                dataGridView1.DataSource = customers;
+            }
+            catch(Exception ex)
+            {
+                ex.Message.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
